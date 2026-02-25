374c54dcc9445dafd3916f8253c36450f8e0addb
Your Name <you@example.com>
2026-02-25 16:02:16 +0500

    Removed Files

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -5,7 +5,7 @@
 
     <title>Hanza Akhlaq's - Portfolio</title>
     <!-- Bootstrap -->
-    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
